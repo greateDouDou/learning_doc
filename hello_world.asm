c9e37b60178d436cc2970cd4d6f2e4adaaba9a2b
Meng Tangman <mengtangman@outlook.com>
2024-09-28 15:11:35 +0800

    add call to ReadConsole

--- hello_world.asm
+++ hello_world.asm
@@ -1,22 +1,41 @@
                         includelib              kernel32.lib
 GetStdHandle            proto                   
 WriteConsoleA           proto
+ReadConsoleA            proto
 Console                 equ                     -11
+Keyboard                equ                     -10
 ExitProcess             proto
 
                         .code
 main                    proc
+
+
                         sub                     rsp, 40
 
                         mov                     rcx, Console
                         call                    GetStdHandle
                         mov                     stdout, rax
+                        mov                     rcx, Keyboard
+                        call                    GetStdHandle
+                        mov                     stdin, rax
 
                         mov                     rcx, stdout
-                        lea                     rdx, msg
-                        mov                     r8, lengthof msg
+                        lea                     rdx, pmsg
+                        mov                     r8, lengthof pmsg
                         lea                     r9, nbwr
                         call                    WriteConsoleA
+
+                        mov	                    rcx,stdin
+                        mov                     r8,24
+                        lea	                    rdx,keymsg
+                        lea	                    r9,nbrd
+                        call	                ReadConsoleA
+
+                        lea	                    rdx,keymsg
+                        mov	                    r8,nbrd
+                        lea	                    r9,nbwr
+                        mov                     rcx, stdout
+                        call	                WriteConsoleA
 
                         add                     rsp, 40
                         ;mov                     rcx, 0
@@ -24,9 +43,14 @@ main                    proc
 
 main                    endp
 
+
                         .data
-msg                     byte                    "hello world"
+
+pmsg                    byte                    "Please enter text message: "
+keymsg                  byte                    20 DUP(?)
 stdout                  qword                   ?
 nbwr                    qword                   ?
+nbrd                    qword                   ?
+stdin                   qword                   ?
 
                         end
